bc6b6cce163456823523ea00c459466400319a73
ndrapeau77-SB <ndrapeau77@gmail.com>
2026-04-29 13:30:37 -0400

    Create food-datebase.js

---
+++ food-datebase.js
@@ -0,0 +1,104 @@
+const foodDatabase = [
+  {
+    name: "Oeuf entier",
+    serving: "1 oeuf",
+    grams: 50,
+    calories: 70,
+    protein: 6.3,
+    carbs: 0.4,
+    fiber: 0,
+    sugar: 0.2
+  },
+  {
+    name: "Blanc d'oeuf",
+    serving: "1 blanc",
+    grams: 33,
+    calories: 17,
+    protein: 3.6,
+    carbs: 0.2,
+    fiber: 0,
+    sugar: 0.2
+  },
+  {
+    name: "Banane",
+    serving: "100 g",
+    grams: 100,
+    calories: 89,
+    protein: 1.1,
+    carbs: 22.8,
+    fiber: 2.6,
+    sugar: 12.2
+  },
+  {
+    name: "Pomme",
+    serving: "1 moyenne",
+    grams: 182,
+    calories: 95,
+    protein: 0.5,
+    carbs: 25,
+    fiber: 4.4,
+    sugar: 19
+  },
+  {
+    name: "Poitrine de poulet cuite",
+    serving: "100 g",
+    grams: 100,
+    calories: 165,
+    protein: 31,
+    carbs: 0,
+    fiber: 0,
+    sugar: 0
+  },
+  {
+    name: "Riz blanc cuit",
+    serving: "100 g",
+    grams: 100,
+    calories: 130,
+    protein: 2.7,
+    carbs: 28.2,
+    fiber: 0.4,
+    sugar: 0.1
+  },
+  {
+    name: "Brocoli",
+    serving: "100 g",
+    grams: 100,
+    calories: 35,
+    protein: 2.4,
+    carbs: 7.2,
+    fiber: 3.3,
+    sugar: 1.4
+  },
+  {
+    name: "Pain blanc",
+    serving: "1 tranche",
+    grams: 32,
+    calories: 84,
+    protein: 2.7,
+    carbs: 15,
+    fiber: 0.8,
+    sugar: 1.4
+  },
+  {
+    name: "Beurre d'arachide",
+    serving: "1 c. à soupe",
+    grams: 16,
+    calories: 94,
+    protein: 4,
+    carbs: 3.2,
+    fiber: 1,
+    sugar: 1.7
+  },
+  {
+    name: "Lait 2%",
+    serving: "250 ml",
+    grams: 250,
+    calories: 122,
+    protein: 8,
+    carbs: 12,
+    fiber: 0,
+    sugar: 12
+  }
+
+  // CONTINUE AVEC LE RESTE...
+];
